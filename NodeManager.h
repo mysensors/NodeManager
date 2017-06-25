1c47965c5321e353dd15a19b2c1913cd9c955213
user2684 <user2684@users.noreply.github.com>
2017-06-25 16:59:07 +0200

    Fixed gateway crashing on node check-in

--- NodeManager.h
+++ NodeManager.h
@@ -1066,7 +1066,7 @@ class NodeManager {
     int _interrupt_2_pull = -1;
     int _last_interrupt_pin = -1;
     long _timestamp = -1;
-    Sensor* _sensors[MAX_SENSORS] = {0};
+    Sensor* _sensors[MAX_SENSORS+1] = {0};
     bool _ack = false;
     void _process(const char * message);
     void _sleep();
