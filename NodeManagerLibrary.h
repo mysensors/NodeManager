29517dfb5f37e88e48689f65d555bea8b6fd2c16
KooLru <kool@kool.ru>
2018-03-18 20:39:26 +0300

    chip definition section

--- NodeManagerLibrary.h
+++ NodeManagerLibrary.h
@@ -27,22 +27,6 @@
 #define OFF 0
 #define ON 1
 
-// define interrupt pins
-
-#if defined(ARDUINO_ARCH_STM32F0) || defined(ARDUINO_ARCH_STM32F1) || defined(ARDUINO_ARCH_STM32F3) || defined(ARDUINO_ARCH_STM32F4) || defined(ARDUINO_ARCH_STM32L4)
-#define INTERRUPT_PIN_1 PB8
-#define INTERRUPT_PIN_2 2
-#else
-#define INTERRUPT_PIN_1 3
-#define INTERRUPT_PIN_2 2
-#endif
-
-// define eeprom addresses
-#define EEPROM_SLEEP_SAVED 0
-#define EEPROM_SLEEP_1 5
-#define EEPROM_SLEEP_2 6
-#define EEPROM_SLEEP_3 7
-#define EEPROM_USER_START 100
 
 /***********************************
    Chip type
@@ -70,6 +54,24 @@
 #if !defined(CHIP_ESP8266) && !defined(CHIP_STM32) && !defined(CHIP_NRF5)
   #define CHIP_AVR
 #endif
+
+// define interrupt pins
+
+#if defined(CHIP_STM32)
+#define INTERRUPT_PIN_1 PB8
+#define INTERRUPT_PIN_2 2
+#else
+#define INTERRUPT_PIN_1 3
+#define INTERRUPT_PIN_2 2
+#endif
+
+// define eeprom addresses
+#define EEPROM_SLEEP_SAVED 0
+#define EEPROM_SLEEP_1 5
+#define EEPROM_SLEEP_2 6
+#define EEPROM_SLEEP_3 7
+#define EEPROM_USER_START 100
+
 
 /***********************************
    Default configuration settings
